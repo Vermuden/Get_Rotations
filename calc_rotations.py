b910e601d084d17c0e57bf18170aeb3f0bd877f6
Vermuden <david.mackel@outlook.de>
2025-12-18 10:54:00 +0100

    new version with new cameras & fixes with calc distances.

--- calc_rotations.py
+++ calc_rotations.py
@@ -9,12 +9,6 @@ def advance_film(distance, diameter, thickness, counter):
     total_degrees = 0
     while distance > 0:
         step_turns = step_size  # Turns per step
-        
-        # Calc step distance with circumrence at start of step, less precise
-        #circumference = math.pi * diameter
-        # step_distance = circumference * step_size
-
-        # Calc step distance with circumfrence at middle of step, more precise
         next_diameter = diameter - 2 * step_size * thickness
         mid_diameter = (diameter + next_diameter) / 2
         step_distance = math.pi * mid_diameter * step_size
@@ -31,14 +25,14 @@ def advance_film(distance, diameter, thickness, counter):
         if (counter % steps_percent ) == 0:
             percent = counter / steps_percent
             filled_length = int(counter / steps_percent)
-            bar = '#' * filled_length + '-' * (100-filled_length)
+            bar = '#' * filled_length  + '.' * (100-filled_length)
             sys.stdout.write(f"\r {bar} {round(percent,0)}%")
             sys.stdout.flush()
             
 
     return total_turns, total_degrees, diameter, counter
 
-print("Enter 1 for 6x6, 2 for 6x12, 3 for 6x3: ")
+print("Enter 1 for 6x6, 2 for 6x12, 3 for 6x3, 4 for 6x12 (Gideon), 5 for 6x6 Nona Wide: ")
 
 while True:
     try:
@@ -50,37 +44,47 @@ while True:
 initial_diameter = 21.6  # mm
 film_thickness_paper = 0.1  # mm
 film_thickness_total = 0.22  # mm
-update_steps = 10000.0 
+update_steps = 10000000.0 
 relative_degrees = 370
 counter = 0
 
-if selection == 1:
+if selection == 1: # 6x6
     initial_advance_paper = 268  # mm war 337.5
     initial_advance_film = 66 # mm
     frame_distance = 64  # mm
     num_frames = 12
     total_steps = 178259228
     steps_percent = 1782592
-    
-elif selection == 2:
+elif selection == 2: # 6x12 v0.1
     initial_advance_paper = 296  # mm war 337.5
     initial_advance_film = 100 # mm
     frame_distance = 128  # mm
     num_frames = 6
     total_steps = 176887976
     steps_percent = 1768879
-elif selection == 3:
+elif selection == 3: # 6x3
     initial_advance_paper = 268  # mm war 337.5
     initial_advance_film = 52 # mm
     frame_distance = 33  # mm
     num_frames = 24
     total_steps = 187244373
     steps_percent = 1872443 
-
-
-   
+elif selection == 4: # 612 Gideon
+    initial_advance_paper = 188  # mm war 337.5
+    initial_advance_film = 35 + 128 # mm
+    frame_distance = 128  # mm
+    num_frames = 6
+    total_steps = 168734218
+    steps_percent = 1687342
+elif selection == 5: #Nona 2 (Wide)
+    initial_advance_paper = 188 # mm war 337.5
+    initial_advance_film = 35 + 60 # mm
+    frame_distance = 66  # mm
+    num_frames = 12
+    total_steps = 173491191
+    steps_percent = 1734912
 else:
-    sys.exit("Number not 1 or 2")
+    sys.exit("Enter a number between 1 and 4.")
 
 diameter = initial_diameter
 turns_list = []
